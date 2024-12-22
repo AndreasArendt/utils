1a6e66052af02e37892afd4f4d44776b970716f0
aarendt <andi.arendt@icloud.com>
2024-12-22 20:03:38 +0100

    add greedy lagrange interp

---
+++ matlab/lagrange_interp.m
@@ -0,0 +1,18 @@
+function p = lagrange_interp(x, y, x_eval)
+    assert(numel(x) == numel(y), 'x and y must have the same length');
+    
+    p = 0;
+    for ii = 1:numel(x)
+        L = 1;
+        for jj = 1:numel(x)
+            % skip this term
+            if jj == ii
+                continue;
+            end
+
+            L = L * (x_eval-x(jj))/(x(ii)-x(jj));
+        end
+        
+        p = p + y(ii)*L;
+    end
+end
