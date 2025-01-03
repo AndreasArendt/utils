35b332bc9f9065a4ce9eb5683d9fdfacaff0b5be
aarendt <andi.arendt@icloud.com>
2025-01-03 19:14:47 +0100

    add new estimation namespace

---
+++ matlab/+estimation/lls.m
@@ -0,0 +1,22 @@
+function [x, P] = lls(A, y, options)
+
+    % Parse input arguments and set default values
+    arguments
+        A double                  % Design matrix
+        y double                  % Observation vector
+        options.R double = eye(size(y, 1)) % Weight matrix, defaults to identity
+    end
+    
+    % Input validation
+    [m, ~] = size(A); % Dimensions of A
+    assert(size(y, 1) == m, 'The number of rows in A must match the length of y.')
+    assert(isequal(size(options.R),  [m, m]), 'The weight matrix R must be square and of size equal to the number of rows in A.')
+    
+    % Compute the weighted least squares solution
+    R_inv = options.R \ eye(size(options.R)); % Numerically stable computation of R_inv
+    normal_matrix = A' * R_inv * A;           % Normal matrix
+    x = normal_matrix \ (A' * R_inv * y);     % Solve normal equations with stabilityend
+
+    % Compute covariance matrix
+    P = inv(normal_matrix);          % Covariance of the estimated parameters    
+end
