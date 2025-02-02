358f329c042f1e5d1b4c16519ad5e0f317fc21c8
aarendt <andi.arendt@icloud.com>
2025-02-02 16:39:08 +0100

    use vecnorm instead of sqrt

--- matlab/Vector.m
+++ matlab/Vector.m
@@ -7,7 +7,7 @@ classdef Vector < handle
             dx = (x1 - x2);
             dy = (y1 - y2);
             dz = (z1 - z2);
-            d12 = sqrt( dx.^2 + dy.^2 + dz.^2);            
+            d12 = vecnorm([dx dy dz],2,2);
         end
 
         function d12 = DistanceVector_3D(x1, y1, z1, x2, y2, z2)
@@ -20,7 +20,7 @@ classdef Vector < handle
 
         function e = NormalizedDistanceVector(x1, y1, z1, x2, y2, z2)
             d = Vector.DistanceVector_3D(x1, y1, z1, x2, y2, z2);
-            e = d ./ sqrt(sum(d.^2,2));
+            e = d ./ vecnorm(d,2,2);
         end
     end
 end
