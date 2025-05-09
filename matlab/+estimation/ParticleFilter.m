050bf1195547159f0986e2209c4cbec29324ba4f
aarendt <andi.arendt@icloud.com>
2025-05-09 18:32:52 +0200

    add particle filter; move kalman filter to estimation

---
+++ matlab/+estimation/ParticleFilter.m
@@ -0,0 +1,62 @@
+classdef ParticleFilter < handle
+
+    properties(SetAccess=private, GetAccess=public)
+        NumberParticles
+        NumberStates
+
+        x_t
+        w_t
+    end
+
+    methods
+        function obj = ParticleFilter(numParticles, numStates, x_t0)
+            obj.NumberParticles = numParticles;
+            obj.NumberStates    = numStates;
+            
+            obj.x_t = x_t0;            
+            obj.w_t = ones(numParticles, 1) ./ numParticles;
+        end
+
+        function Predict(obj, u_t, options)                        
+            arguments(Input)
+                obj
+                u_t % Input                
+                options.Q = 0.001;
+            end
+                        
+            r = randn(obj.NumberParticles, obj.NumberStates) .* options.Q;
+            obj.x_t = obj.x_t + u_t + r;
+        end
+
+        function Correct(obj, dy)            
+            obj.w_t = 1./(dy.^2 + 1e-6);
+            obj.w_t = obj.w_t ./ sum(obj.w_t);
+        end
+      
+        function Resample(obj)
+            obj.x_t = obj.LowVarianceSampling(obj.x_t, obj.w_t);
+        end
+
+        % optimized Roulette Wheel Sampling
+        function x_new = LowVarianceSampling(obj, x_t, w_t)
+            x_new = NaN(obj.NumberParticles, obj.NumberStates);
+
+            % Normalize weights
+            w = w_t ./ sum(w_t);
+
+            % Initialize variables
+            r = rand / obj.NumberParticles;
+            c = w(1);
+            ii = 1;
+
+            for m = 1:obj.NumberParticles
+                U = r + (m - 1) / obj.NumberParticles;
+                while U > c
+                    ii = ii + 1;
+                    c = c + w(ii);
+                end
+                x_new(m, :) = x_t(ii, :);
+            end
+        end
+    end
+end
