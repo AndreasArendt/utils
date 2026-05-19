edd91785e3dc156d777bf93815141031536bf5c2
aarendt <12445570+AndreasArendt@users.noreply.github.com>
2026-05-19 20:30:26 +0200

    use second order approx in rodrigues

--- matlab/+Transformation/+Lie/Rodrigues.m
+++ matlab/+Transformation/+Lie/Rodrigues.m
@@ -3,12 +3,12 @@ function R = Rodrigues(rot_xyz)
 % see: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
 
 theta = vecnorm(rot_xyz, 2,2);
+K = Vector.skew(rot_xyz);
 
 if theta < 1e-12
-    % First-order approximation
-    R = eye(3) + Vector.skew(phi);
+    % Second-order approximation    
+    R = eye(3) + K + 0.5*K*K;
     return;
 end
 
-K = Vector.skew(rot_xyz);
 R = eye(3) + sin(theta)/theta * K + ((1-cos(theta))/theta^2)*K*K;
