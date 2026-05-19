bee5b9b8b534c9acb4fc002b670e05fb78426ed0
aarendt <12445570+AndreasArendt@users.noreply.github.com>
2026-05-19 19:55:14 +0200

    add skew matrix

--- matlab/Vector.m
+++ matlab/Vector.m
@@ -22,6 +22,22 @@ classdef Vector < handle
             d = Vector.DistanceVector_3D(x1, y1, z1, x2, y2, z2);
             e = d ./ vecnorm(d,2,2);
         end
+
+        function S = skew(v)
+            assert(size(v,2) == 3, "allow only [n x 3] vectors")
+        
+            n = size(v,1);
+            S = zeros(3,3,n);
+        
+            S(1,2,:) = -v(:,3);
+            S(1,3,:) =  v(:,2);
+        
+            S(2,1,:) =  v(:,3);
+            S(2,3,:) = -v(:,1);
+        
+            S(3,1,:) = -v(:,2);
+            S(3,2,:) =  v(:,1);
+        end
     end
 end
 
